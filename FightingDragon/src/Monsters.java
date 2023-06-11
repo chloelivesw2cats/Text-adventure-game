3c6bf4a40539e1e3eea3328e4a5f1cebe45ded67
Chloe <130580596+chloelivesw2cats@users.noreply.github.com>
2023-06-12 02:34:07 +0700

    Update and rename MonsterEncounter.java to Monsters.java

---
+++ FightingDragon/src/Monsters.java
@@ -0,0 +1,19 @@
+public class Monsters {
+    private int HP;
+    private int Damage;
+
+    public Monsters(int HP, int damage) {
+        this.HP = HP;
+        Damage = damage;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public int getDamage() {
+        return Damage;
+    }
+    
+
+}
