f81b46ed83884e081aa1c9a3f587764dff0a3ea0
Chloe <130580596+chloelivesw2cats@users.noreply.github.com>
2023-06-12 02:34:54 +0700

    Create TheBoss.java

---
+++ FightingDragon/src/TheBoss.java
@@ -0,0 +1,18 @@
+public class TheBoss {
+        private int HP = 100;
+        private int Damage = 10;
+        public boolean isAlive(){
+        return HP > 0;
+    }
+        public int getHP() {
+            return HP;
+        }
+
+        public int getDamage() {
+            return Damage;
+        }
+
+
+    }
+
+}
