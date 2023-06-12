009a88e33f4b3576c9fdaefb8dc7fa40ae473de0
Chloe <130580596+chloelivesw2cats@users.noreply.github.com>
2023-06-12 15:19:54 +0700

    Update TheHero.java

--- FightingDragon/src/TheHero.java
+++ FightingDragon/src/TheHero.java
@@ -1,28 +1,20 @@
 public class TheHero {
     private int HP;
-    private int Stamina;
+    private int maxHP;
     private int AttackPower;
+    private int Gold;
+    private int XP;
     private String name;
 
     public int getHP() {
         return HP;
     }
 
-    public int getStamina() {
-        return Stamina;
-    }
-
     public int getAttackPower() {
         return AttackPower;
     }
 
-    public void setHP(int HP) {
-        this.HP = HP;
-    }
-
-    public void setStamina(int stamina) {
-        Stamina = stamina;
-    }
+    public void setHP(int HP) {this.HP = HP;    }
 
     public void setAttackPower(int attackPower) {
         AttackPower = attackPower;
@@ -36,19 +28,54 @@ public class TheHero {
         this.name = name;
     }
 
+    public int getGold() { return Gold; }
+
+    public void setGold(int gold) { Gold = gold; }
+
+    public int getMaxHP() {return maxHP;   }
+
+    public void setMaxHP(int maxHP) {this.maxHP = maxHP;   }
+
+    public int getXP() { return XP;    }
+
+    public void setXP(int XP) {   this.XP = XP;    }
+
+
     public boolean isAlive(){
         return HP > 0;
     }
-    public void heal(int x){
-        HP += x;
+    public void beingAttacked(int x){
+        HP -= x;
+        System.out.println("You suffered from damage, your current HP is: " + HP);
+    }
+
+    public void heal(){
+        HP = maxHP;
         System.out.println("You feel much better already, your current HP is: " + HP);
     }
-    public void rest(){
-        Stamina = 10;
-        System.out.println("Your stamina has been topped off");
+
+    public void gainXP(int x){
+        XP += x;
+        System.out.println("You gained some XP. Your current XP is: " + XP +"/100");
     }
-    public void levelup(int z){
-        AttackPower += z;
+
+    public void levelUp() {
+        AttackPower += 5;
+        maxHP += 10;
+        HP = maxHP;
         System.out.println("You feel stronger, your current AttackPower power is: " + AttackPower);
+        System.out.println("You feel stronger, your new HP is: " + maxHP);
+    }
+
+    public void gainGold(int x){
+        Gold += x;
+        System.out.println("You gained " + x +" golds");
+        System.out.println("Your current amount of Gold is: " + Gold);
+    }
+
+    public void useGold(int x){
+        Gold -= x;
+        System.out.println("You used " + x +" golds");
+        System.out.println("Your current amount of Gold is: " + Gold);
     }
 }
